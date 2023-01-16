bb21ce6cfe6d827f9b032a4e02727f46482304cc
Lonnie Souder II <lonnie2@souderfamily.net>
2023-01-15 23:23:40 -0500

    feature: able to display spheres with text in a circle (sizing/spacing/layout remain an issue for another day

--- app.js
+++ app.js
@@ -2,26 +2,58 @@
  * AR.js Sandbox App
  */
 
+/**
+ * Queries back-facing cameras from device
+ * @returns Promise containing an array of back-facing, camera, media devices
+ */
 async function GetAvailableCameras() {
     const IsBackCamera = device => device.kind == "videoinput" && device.label.includes("facing back")
     return navigator.mediaDevices.enumerateDevices()
         .then(devices => devices.filter(IsBackCamera));
 }
 
-function CameraSelectOnChange() {
+/**
+ * Event handler for Camera Selector OnChange event.
+ * This should change the camera to the newly chosen camera
+ */
+function HandleCameraSelectOnChange() {
     let cameraSelect = document.getElementById("camera-select");
     let video = document.getElementById("arjs-video");
     if (typeof video.srcObject !== "undefined") {
         video.srcObject.getTracks().forEach(track => track.stop());
     }
     let constraints = {
-        video: {deviceId: { exact: cameraSelect.value }},
+        video: { deviceId: { exact: cameraSelect.value } },
         audio: false
     };
 
     navigator.mediaDevices.getUserMedia(constraints)
         .then(stream => video.srcObject = stream)
         .catch(error => console.log(error));
+}
+
+
+/**
+ * Creates an AFrame sphere element with label, `text` parented to `parent` at `position`
+ * @param {Element} parent Parent of object to be created
+ * @param {Number[]} position 3D Euclidean coordinates of the new object
+ * @param {string} text Label to display inside the object
+ */
+function CreateChildTextSphere(parent, position, text) {
+    let sphere = document.createElement("a-entity");
+    sphere.setAttribute("geometry", { primitive: "sphere" });
+    sphere.setAttribute("material", { color: "#b3b3cc", opacity: 0.5 });
+    sphere.object3D.position.set(...position);
+    sphere.object3D.scale.set(0.25, 0.25, 0.25);
+    parent.appendChild(sphere);
+
+    let sphereText = document.createElement("a-entity");
+    sphereText.setAttribute("text", {
+        value: text,
+        align: "center"
+    });
+    sphereText.object3D.scale.set(8, 8, 8);
+    sphere.appendChild(sphereText);
 }
 
 
@@ -39,6 +71,17 @@ function main() {
             }
         })
         .catch(error => console.log("Unable to generate camera selector options"));
+
+    const skills = ['C++', 'CUDA', 'WhateverThisIs'];
+    const marker = document.getElementsByTagName("a-marker")[0];
+    for (let i = 0; i < skills.length; ++i) {
+        const radians = 2 * Math.PI * (i / skills.length);
+        const distanceFromCenter = 0.5;
+        CreateChildTextSphere(
+            marker,
+            [Math.cos(radians) * distanceFromCenter, 0, Math.sin(radians) * distanceFromCenter],
+            skills[i]);
+    }
 }
 
 
